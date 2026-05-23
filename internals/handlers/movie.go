957482177fe5e6e52db6bf732e7c30abe0b7654e
Chirag Lala <chirag42001@gmail.com>
2026-05-23 12:02:56 +0000

    Improving error handling and http responses

--- internals/handlers/movie.go
+++ internals/handlers/movie.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -11,18 +10,50 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type response struct {
+	Status  int `json:"status"`
+	Message any `json:"data"`
+}
+
 func CreateMovie(w http.ResponseWriter, r *http.Request) {
 	var m models.Movie
-	json.NewDecoder(r.Body).Decode(&m)
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576) //This will return a error response as soon as response body size becomes more than 1 MB
+
+	dec := json.NewDecoder(r.Body)
+
+	dec.DisallowUnknownFields()
+
+	if err := dec.Decode(&m); err != nil {
+		sendResponse(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	//normally should be returning error and we should cacth it here and return error response, but here we are suing in memeory store,  So that is not needed
 	store.AddMovie(m)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Success")
+	sendResponse(w, http.StatusCreated, "Movie Created Successfully")
 }
 
 func GetMovie(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	id, _ := strconv.Atoi(vars["id"])
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		sendResponse(w, http.StatusBadRequest, "Bad Request")
+		return
+	}
+
 	returnBody := store.ReturnMovie(id)
-	json.NewEncoder(w).Encode(returnBody)
-	w.WriteHeader(http.StatusOK)
+
+	if returnBody.Title == "" {
+		sendResponse(w, http.StatusNotFound, "No such movie in our collection")
+		return
+	}
+
+	sendResponse(w, http.StatusFound, returnBody)
+}
+
+func sendResponse(w http.ResponseWriter, statusCode int, message any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(response{Status: statusCode, Message: message})
 }
